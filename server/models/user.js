dfc6c35e29cdb17f243b161cc2cceb6af6f06c80
Ruben E Acosta <eldragondera1@gmail.com>
2019-01-30 23:43:43 -0500

    modelo de usuario

---
+++ server/models/user.js
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from 'mongoose'
+import uniqueValidator from 'mongoose-unique-validator'
+
+const UserSchema = new Schema({
+  firstName: { type: String, required: true},
+  surname: { type: String, required: true},
+  email:: { type: String, required: true, unique: true, index: true},
+  password: { type: String, required: true}
+})
+
+UserSchema.plugin(uniqueValidator)
+
+const User = mongoose.model('User', UserSchema)
+
+export default User
