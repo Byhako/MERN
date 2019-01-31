ab4978c41e401016f131674f19a88314fc583f87
Ruben E Acosta <eldragondera1@gmail.com>
2019-01-31 10:20:19 -0500

    coneccion con mongo

--- server/config/index.js
+++ server/config/index.js
@@ -1,1 +1,2 @@
 export const clave = 'miclave'
+export const mongoUrl = 'mongodb://localhost:27017/overflow'
--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 import Debug from 'debug'
 import app from './app'
+import mongoose from 'mongoose'
+import { mongoUrl } from './config'
 
 const PORT = 3000
 const debug = new Debug('server:index')
 
 
-app.listen(PORT, () => {
-  debug(`Server running in port ${PORT}`)
-})
+async function start () {
+  await mongoose.connect(mongoUrl, { useNewUrlParser: true })
+  debug('Connect MongoDb Ok.')
+  app.listen(PORT, () => {
+    debug(`Server running in port ${PORT}`)
+  })
+}
+
+start()
